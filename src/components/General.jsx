c182f0f1f4414ec20f3f26e272b3c6c073fcdcc0
Carmen Morata <cmoratagarcia@gmail.com>
2026-05-04 10:59:03 -0700

    Add form data state management to GeneralInfo component

--- src/components/General.jsx
+++ src/components/General.jsx
@@ -2,6 +2,11 @@ import { useState } from "react";
 
 function GeneralInfo() {
   const [isEditing, setIsEditing] = useState(true); //tell the component whether it's in Editing or Display mode
+  const [formData, setFormData] = useState({
+    name: "",
+    phone: "",
+    email: "",
+  });
 
   return <div>General Info</div>;
 }
