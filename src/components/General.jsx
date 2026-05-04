062bc1fd124404ac23e5f0480046ace0e3cb6c27
Carmen Morata <cmoratagarcia@gmail.com>
2026-05-04 10:58:04 -0700

    Add state management to GeneralInfo component for editing mode

--- src/components/General.jsx
+++ src/components/General.jsx
@@ -1,4 +1,8 @@
+import { useState } from "react";
+
 function GeneralInfo() {
+  const [isEditing, setIsEditing] = useState(true); //tell the component whether it's in Editing or Display mode
+
   return <div>General Info</div>;
 }
 
